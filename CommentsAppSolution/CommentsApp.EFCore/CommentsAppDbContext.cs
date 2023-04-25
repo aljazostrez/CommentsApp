c6e1f0a199c4d9ca85cc61f8a017951e55356e28
Aljaž Ostrež <aljaz.ostrez@spica.com>
2023-04-26 01:26:49 +0200

    Add EFCore project, where we configure connection to the DB and specify tables. Also add sqlite db as a local file in WebAPI folder.

---
+++ CommentsAppSolution/CommentsApp.EFCore/CommentsAppDbContext.cs
@@ -0,0 +1,29 @@
+﻿using CommentsApp.Core;
+using Microsoft.EntityFrameworkCore;
+
+namespace CommentsApp.EFCore
+{
+    public class CommentsAppDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+
+        // this is just a demonstration of connecting to DB, so we use sqlite db as local file.
+        // DB should be hosted somewhere and connection string should not be hardcoded, but configured
+        // in appsettings.json or somewhere else (eg. MS Azure Key Vault)
+        private string _dbPath { get; }
+
+        public CommentsAppDbContext()
+        {
+            var startupPath = Directory.GetCurrentDirectory();
+            _dbPath = Path.Join(startupPath, "commentsAppDb.db");
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            if (!options.IsConfigured)
+                options.UseSqlite($"Data Source={_dbPath}");
+
+        }
+    }
+}
