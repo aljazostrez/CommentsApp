01617071a4bb644c54f3c9a8ae89571a78a1394c
Aljaž Ostrež <aljaz.ostrez@spica.com>
2023-04-26 01:15:00 +0200

    Add Core project, that contains models

---
+++ CommentsAppSolution/CommentsApp.Core/Comment.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CommentsApp.Core
+{
+    public class Comment
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public string Text { get; set; }
+
+        [ForeignKey(nameof(UserId))] public User User { get; set; }
+    }
+}
